56bcd9e2ee1cd0807acf183ad5945015a8a1c5b9
nariatsu1202 <atsuki02jojo@gmail.com>
2018-06-18 17:11:34 +0900

    Initial Commit

---
+++ job-hunting-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  job-hunting-app
+//
+//  Created by 成田篤基 on 2018/06/18.
+//  Copyright © 2018年 Atsuki Narita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
